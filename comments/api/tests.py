48ad41612173a54fe7daebf05c51f9039d94bba3
Yichuan <wsxdwang@gmail.com>
2022-05-07 23:22:18 -0500

    Add comment model and create method.

---
+++ comments/api/tests.py
@@ -0,0 +1,54 @@
+from testing.testcases import TestCase
+from rest_framework.test import APIClient
+
+
+COMMENT_URL = '/api/comments/'
+
+
+class CommentApiTests(TestCase):
+
+    def setUp(self):
+        self.anonymous_client = APIClient()
+        self.linghu = self.create_user('linghu')
+        self.linghu_client = APIClient()
+        self.linghu_client.force_authenticate(self.linghu)
+        self.dongxie = self.create_user('dongxie')
+        self.dongxie_client = APIClient()
+        self.dongxie_client.force_authenticate(self.dongxie)
+
+        self.tweet = self.create_tweet(self.linghu)
+
+    def test_create(self):
+        # 匿名不可以创建
+        response = self.anonymous_client.post(COMMENT_URL)
+        self.assertEqual(response.status_code, 403)
+
+        # 啥参数都没带不行
+        response = self.linghu_client.post(COMMENT_URL)
+        self.assertEqual(response.status_code, 400)
+
+        # 只带 tweet_id 不行
+        response = self.linghu_client.post(COMMENT_URL, {'tweet_id': self.tweet.id})
+        self.assertEqual(response.status_code, 400)
+
+        # 只带 content 不行
+        response = self.linghu_client.post(COMMENT_URL, {'content': '1'})
+        self.assertEqual(response.status_code, 400)
+
+        # content max length 140
+        response = self.linghu_client.post(COMMENT_URL, {
+            'tweet_id': self.tweet.id,
+            'content': '1' * 141,
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual('content' in response.data['errors'], True)
+
+        # both tweet_id and content are required field
+        response = self.linghu_client.post(COMMENT_URL, {
+            'tweet_id': self.tweet.id,
+            'content': '1',
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['user']['id'], self.linghu.id)
+        self.assertEqual(response.data['tweet_id'], self.tweet.id)
+        self.assertEqual(response.data['content'], '1')
