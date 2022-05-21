b8463c0e93fd25ba4b0d3c47af84c6a187d362ec
Yichuan <wsxdwang@gmail.com>
2022-05-21 01:21:57 -0500

    Add update and delete method for comments

--- testing/testcases.py
+++ testing/testcases.py
@@ -1,9 +1,17 @@
 from django.test import TestCase as DjangoTestCase
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+
+from comments.models import Comment
 from tweets.models import Tweet
 
 
 class TestCase(DjangoTestCase):
+    def anonymous_client(self):
+        if hasattr(self, '_anonymous_client'):
+            return self._anonymous_client
+        self._anonymous_client = APIClient()
+        return self._anonymous_client
 
     def create_user(self, username, email=None, password=None):
         if email is None:
@@ -17,3 +25,8 @@ class TestCase(DjangoTestCase):
         if content is None:
             content = 'default tweet content'
         return Tweet.objects.create(user=user, content=content)
+
+    def create_comment(self, user, tweet, content=None):
+        if content is None:
+            content = 'default comment content'
+        return Comment.objects.create(user=user, tweet=tweet, content=content)
