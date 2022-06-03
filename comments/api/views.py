3273bca91a18ecbbbd4949c674562ea19275a2ba
Yichuan <wsxdwang@gmail.com>
2022-06-02 19:34:36 -0500

    Add comment list api
    Add comment list api

--- comments/api/views.py
+++ comments/api/views.py
@@ -17,6 +17,7 @@ class CommentViewSet(viewsets.GenericViewSet):
     """
     serializer_class = CommentSerializerForCreate
     queryset = Comment.objects.all()
+    filterset_fields = ('tweet_id',)
 
     def get_permissions(self):
         if self.action =='create':
@@ -65,3 +66,20 @@ class CommentViewSet(viewsets.GenericViewSet):
         comment.delete()
         # DRF default status code is 204 no content
         return Response({'success': True}, status=status.HTTP_200_OK)
+
+    def list(self, request, *args, **kwargs):
+        if 'tweet_id' not in request.query_params:
+            return Response(
+                {
+                    'message': 'missing tweet_id in request',
+                    'success': False,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        queryset = self.get_queryset()
+        comments = self.filter_queryset(queryset).order_by('created_at')
+        serializer = CommentSerializer(comments, many=True)
+        return Response(
+            {'comments': serializer.data},
+            status = status.HTTP_200_OK,
+        )
