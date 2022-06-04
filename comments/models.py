8bc2c0a6f5d5e7a6f397c02898693899eca13073
Yichuan <wsxdwang@gmail.com>
2022-06-04 09:44:56 -0500

    Add like model and admin
    Add like model and admin

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tweets.models import Tweet
+from django.contrib.contenttypes.models import ContentType
+from likes.models import Like
+
 
 
 class Comment(models.Model):
@@ -17,3 +20,9 @@ class Comment(models.Model):
     def __str__(self):
         return f'{self.created_at} - {self.user} says {self.content} at tweet {self.tweet_id}'
 
+    @property
+    def like_set(self):
+        return Like.objects.filter(
+            content_type=ContentType.objects.get_for_model(Comment),
+            object_id = self.id,
+        ).order_by('created_at')
